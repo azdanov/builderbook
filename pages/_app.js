7702d206734a08721829d9fe59ca98d4cba832a2
Anton Ždanov <anton@azdanov.dev>
2022-09-16 22:20:37 +0300

    feat: finish chapter 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css';
+import createCache from "@emotion/cache";
+import { CacheProvider, ThemeProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "next/app";
+import Head from "next/head";
+import PropTypes from "prop-types";
+import React from "react";
+import Header from "../components/Header";
+import { theme } from "../lib/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <CacheProvider value={createCache({ key: "css" })}>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Header {...pageProps} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    );
+  }
 }
+
+MyApp.propTypes = propTypes;
 
 export default MyApp;
